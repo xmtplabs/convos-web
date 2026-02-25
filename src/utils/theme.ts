221c783a7dacc4ffdadb31e51bc35df3a4307576
Ry Racherbaumer <ry@xmtp.com>
2026-02-24 20:35:49 -0600

    Add xxl size text to theme

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -4,9 +4,11 @@ export const theme = createTheme({
   primaryColor: "dark",
   fontSizes: {
     xxs: "calc(0.6875rem * var(--mantine-scale))",
+    xxl: "calc(2rem * var(--mantine-scale))",
   },
   lineHeights: {
     xxs: "1.2",
+    xxl: "1.25",
   },
   spacing: {
     xxs: "calc(0.5rem * var(--mantine-scale))",
