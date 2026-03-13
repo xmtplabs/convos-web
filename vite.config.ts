cae46451c30f0356d370e73d3dedd338e4f5e78f
Ry Racherbaumer <ry@xmtp.com>
2026-03-13 09:59:15 -0500

    Upgrade build tools

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,18 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   define: {
     __SW_VERSION__: JSON.stringify(Date.now().toString(36)),
   },
-  worker: {
-    plugins: () => [viteTsConfigPaths({ projects: ["./tsconfig.json"] })],
-  },
   optimizeDeps: {
     exclude: ["@xmtp/browser-sdk"],
   },
-  plugins: [
-    devtools(),
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-    tanstackStart(),
-    nitro(),
-    react(),
-  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [devtools(), tanstackStart(), nitro(), react()],
 });
 
 export default config;
